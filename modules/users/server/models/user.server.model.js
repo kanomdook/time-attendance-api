8a38e77b8853cad2d5897e726da4171e111fcb1b
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-06-15 10:13:59 +0700

    update

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -78,6 +78,10 @@ var UserSchema = new Schema({
     type: Schema.ObjectId,
     ref: 'Employeeprofile'
   },
+  company: {
+    type: Schema.ObjectId,
+    ref: 'Company'
+  },
   provider: {
     type: String,
     required: 'Provider is required'
