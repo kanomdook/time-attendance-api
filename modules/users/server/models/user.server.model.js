8d73fc6dc91e80ab2579642ef84d9222304c9b3f
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-06-22 16:24:40 +0700

    update

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -72,7 +72,7 @@ var UserSchema = new Schema({
   },
   profileImageURL: {
     type: String,
-    default: 'modules/users/client/img/profile/default.png'
+    default: 'default.png'
   },
   employeeprofile: {
     type: Schema.ObjectId,
