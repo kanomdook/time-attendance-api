295c946d20b82cf3a1f3987dc9860ca5b9662378
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-06-22 12:58:40 +0700

    update

--- modules/checkins/server/controllers/checkins.server.controller.js
+++ modules/checkins/server/controllers/checkins.server.controller.js
@@ -221,7 +221,7 @@ exports.listByCompany = function (req, res) {
         message: errorHandler.getErrorMessage(err)
       });
     } else {
-      var checkinByCompany = checkin.filter(function (obj) { return obj.user.employeeprofile.company == req.user.company })
+      var checkinByCompany = checkin.filter(function (obj) { return obj.user.employeeprofile.company._id == req.user.company._id })
       res.jsonp(checkinByCompany);
     }
   });
