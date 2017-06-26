c40dd00f2a904972521fde0fbc76e6049084f0c5
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-06-26 11:34:25 +0700

    update reportdealy

--- modules/reportdailies/server/controllers/reportdailies.server.controller.js
+++ modules/reportdailies/server/controllers/reportdailies.server.controller.js
@@ -4,92 +4,93 @@
  * Module dependencies.
  */
 var path = require('path'),
-  mongoose = require('mongoose'),
-  Reportdaily = mongoose.model('Reportdaily'),
-  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
-  _ = require('lodash');
+    mongoose = require('mongoose'),
+    Reportdaily = mongoose.model('Reportdaily'),
+    Checkin = mongoose.model('Checkin'),
+    errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
+    _ = require('lodash');
 
 /**
  * Create a Reportdaily
  */
 exports.create = function(req, res) {
-  var reportdaily = new Reportdaily(req.body);
-  reportdaily.user = req.user;
+    var reportdaily = new Reportdaily(req.body);
+    reportdaily.user = req.user;
 
-  reportdaily.save(function(err) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.jsonp(reportdaily);
-    }
-  });
+    reportdaily.save(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(reportdaily);
+        }
+    });
 };
 
 /**
  * Show the current Reportdaily
  */
 exports.read = function(req, res) {
-  // convert mongoose document to JSON
-  var reportdaily = req.reportdaily ? req.reportdaily.toJSON() : {};
+    // convert mongoose document to JSON
+    var reportdaily = req.reportdaily ? req.reportdaily.toJSON() : {};
 
-  // Add a custom field to the Article, for determining if the current User is the "owner".
-  // NOTE: This field is NOT persisted to the database, since it doesn't exist in the Article model.
-  reportdaily.isCurrentUserOwner = req.user && reportdaily.user && reportdaily.user._id.toString() === req.user._id.toString();
+    // Add a custom field to the Article, for determining if the current User is the "owner".
+    // NOTE: This field is NOT persisted to the database, since it doesn't exist in the Article model.
+    reportdaily.isCurrentUserOwner = req.user && reportdaily.user && reportdaily.user._id.toString() === req.user._id.toString();
 
-  res.jsonp(reportdaily);
+    res.jsonp(reportdaily);
 };
 
 /**
  * Update a Reportdaily
  */
 exports.update = function(req, res) {
-  var reportdaily = req.reportdaily;
+    var reportdaily = req.reportdaily;
 
-  reportdaily = _.extend(reportdaily, req.body);
+    reportdaily = _.extend(reportdaily, req.body);
 
-  reportdaily.save(function(err) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.jsonp(reportdaily);
-    }
-  });
+    reportdaily.save(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(reportdaily);
+        }
+    });
 };
 
 /**
  * Delete an Reportdaily
  */
 exports.delete = function(req, res) {
-  var reportdaily = req.reportdaily;
+    var reportdaily = req.reportdaily;
 
-  reportdaily.remove(function(err) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.jsonp(reportdaily);
-    }
-  });
+    reportdaily.remove(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(reportdaily);
+        }
+    });
 };
 
 /**
  * List of Reportdailies
  */
 exports.list = function(req, res) {
-  Reportdaily.find().sort('-created').populate('user', 'displayName').exec(function(err, reportdailies) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.jsonp(reportdailies);
-    }
-  });
+    Reportdaily.find().sort('-created').populate('user', 'displayName').exec(function(err, reportdailies) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(reportdailies);
+        }
+    });
 };
 
 /**
@@ -97,21 +98,61 @@ exports.list = function(req, res) {
  */
 exports.reportdailyByID = function(req, res, next, id) {
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).send({
-      message: 'Reportdaily is invalid'
-    });
-  }
-
-  Reportdaily.findById(id).populate('user', 'displayName').exec(function (err, reportdaily) {
-    if (err) {
-      return next(err);
-    } else if (!reportdaily) {
-      return res.status(404).send({
-        message: 'No Reportdaily with that identifier has been found'
-      });
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({
+            message: 'Reportdaily is invalid'
+        });
     }
-    req.reportdaily = reportdaily;
-    next();
-  });
+
+    Reportdaily.findById(id).populate('user', 'displayName').exec(function(err, reportdaily) {
+        if (err) {
+            return next(err);
+        } else if (!reportdaily) {
+            return res.status(404).send({
+                message: 'No Reportdaily with that identifier has been found'
+            });
+        }
+        req.reportdaily = reportdaily;
+        next();
+    });
+};
+
+exports.reportdailyByDate = function(req, res, next, reportdate) {
+    // body...
+    var newDate = new Date(reportdate);
+    var reportEndDate = null;
+    if (newDate.getMonth() > 10) {
+        reportEndDate = new Date(newDate.getFullYear() + 1 + '-01');
+    } else {
+        reportEndDate = new Date(newDate).setMonth(new Date(newDate).getMonth() + 1);
+    }
+    console.log('{ created: { $gte: ' + newDate + ',$lt: ' + new Date(reportEndDate) + ' } }');
+    Checkin.find({ created: { $gte: newDate, $lt: new Date(reportEndDate) } }).populate({
+        path: 'user',
+        model: 'User',
+        populate: {
+            path: 'employeeprofile',
+            model: 'Employeeprofile',
+            populate: {
+                path: 'company',
+                model: 'Company'
+            }
+        }
+    }).exec(function(err, reportdaily) {
+        if (err) {
+            return next(err);
+        } else if (!reportdaily) {
+            return res.status(404).send({
+                message: 'No Reportdaily with that identifier has been found'
+            });
+        }
+        req.reportdaily = reportdaily;
+        next();
+    });
+};
+
+exports.reportdaily = function(req, res, next) {
+    // body...
+    res.jsonp(req.reportdaily);
+    // res.send('OK');
 };
--- modules/reportdailies/server/routes/reportdailies.server.routes.js
+++ modules/reportdailies/server/routes/reportdailies.server.routes.js
@@ -4,19 +4,25 @@
  * Module dependencies
  */
 var reportdailiesPolicy = require('../policies/reportdailies.server.policy'),
-  reportdailies = require('../controllers/reportdailies.server.controller');
+    reportdailies = require('../controllers/reportdailies.server.controller');
 
 module.exports = function(app) {
-  // Reportdailies Routes
-  app.route('/api/reportdailies').all(reportdailiesPolicy.isAllowed)
-    .get(reportdailies.list)
-    .post(reportdailies.create);
+    // Reportdailies Routes
+    app.route('/api/reportdailies').all(reportdailiesPolicy.isAllowed)
+        .get(reportdailies.list)
+        .post(reportdailies.create);
 
-  app.route('/api/reportdailies/:reportdailyId').all(reportdailiesPolicy.isAllowed)
-    .get(reportdailies.read)
-    .put(reportdailies.update)
-    .delete(reportdailies.delete);
+    app.route('/api/reportdailies/:reportdailyId').all(reportdailiesPolicy.isAllowed)
+        .get(reportdailies.read)
+        .put(reportdailies.update)
+        .delete(reportdailies.delete);
 
-  // Finish by binding the Reportdaily middleware
-  app.param('reportdailyId', reportdailies.reportdailyByID);
+    // Reportdailies Routes
+    app.route('/api/reportdaily/:reportdate')//.all(reportdailiesPolicy.isAllowed)
+        .get(reportdailies.reportdaily);
+
+    // Finish by binding the Reportdaily middleware
+    app.param('reportdate', reportdailies.reportdailyByDate);
+
+    app.param('reportdailyId', reportdailies.reportdailyByID);
 };
