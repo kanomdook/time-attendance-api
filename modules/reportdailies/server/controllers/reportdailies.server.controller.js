75120f285e02b6b43be743494c531586c47bd85b
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-06-26 18:46:41 +0700

    update

--- modules/reportdailies/server/controllers/reportdailies.server.controller.js
+++ modules/reportdailies/server/controllers/reportdailies.server.controller.js
@@ -245,30 +245,11 @@ exports.exportByDate = function(req, res, next) {
 
     //Array of objects representing heading rows (very top) 
     var heading = [
-        [{ value: 'ทดสอบ สวัสดีครับ', style: styles.headerDark }, { value: 'b1', style: styles.headerDark }, { value: 'c1', style: styles.headerDark }],
-        ['a2', 'b2', 'c2'] // <-- It can be only values 
+        [{ value: 'ทดสอบ สวัสดีครับ', style: styles.headerDark },{ value: 'ทดสอบ สวัสดีครับ', style: styles.headerDark }] // <-- It can be only values 
     ];
 
     //Here you specify the export structure 
     var specification = {
-        customer_name: { // <- the key should match the actual data key 
-            displayName: 'Customer', // <- Here you specify the column header 
-            headerStyle: styles.headerDark, // <- Header style 
-            cellStyle: function(value, row) { // <- style renderer function 
-                // if the status is 1 then color in green else color in red 
-                // Notice how we use another cell value to style the current one 
-                return (row.status_id === 1) ? styles.cellGreen : { fill: { fgColor: { rgb: 'FFFF0000' } } }; // <- Inline cell style is possible  
-            },
-            width: 120 // <- width in pixels 
-        },
-        status_id: {
-            displayName: 'Status',
-            headerStyle: styles.headerDark,
-            cellFormat: function(value, row) { // <- Renderer function, you can access also any row.property 
-                return (value === 1) ? 'Active' : 'Inactive';
-            },
-            width: '10' // <- width in chars (when the number is passed as string) 
-        },
         note: {
             displayName: 'Description',
             headerStyle: styles.headerDark,
