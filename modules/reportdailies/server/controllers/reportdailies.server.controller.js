c38d50e24149d67abe0a53977a289ef666bd2863
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-06-26 17:01:34 +0700

    update

--- modules/reportdailies/server/controllers/reportdailies.server.controller.js
+++ modules/reportdailies/server/controllers/reportdailies.server.controller.js
@@ -183,7 +183,7 @@ exports.reportdailyByDate = function(req, res, next, reportdate) {
                                 lng: i.locationOut.lng
                             },
                             device: i.user.deviceID,
-                            distance: null,
+                            distance: getDistanceFromLatLonInKm(i.locationIn.lat, i.locationIn.lng, i.locationOut.lat, i.locationOut.lng),
                             workinghours: null,
                             overtime: null,
                             remark: {
@@ -206,3 +206,20 @@ exports.reportdailyByDate = function(req, res, next, reportdate) {
 exports.reportdaily = function(req, res, next) {
     res.jsonp(req._reportdaily);
 };
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1); // deg2rad below
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180);
+}
