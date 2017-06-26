181b1601cbce56fc6e1ab1d0a6892ca68c9c1d10
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-06-26 15:26:08 +0700

    update

--- modules/reportdailies/client/services/reportdailies.client.service.js
+++ modules/reportdailies/client/services/reportdailies.client.service.js
@@ -1,20 +1,36 @@
 // Reportdailies service used to communicate Reportdailies REST endpoints
-(function () {
-  'use strict';
+(function() {
+    'use strict';
 
-  angular
-    .module('reportdailies')
-    .factory('ReportdailiesService', ReportdailiesService);
+    angular
+        .module('reportdailies')
+        .factory('ReportdailiesService', ReportdailiesService)
+        .service('ReportdailiesDayService', ReportdailiesDayService);
 
-  ReportdailiesService.$inject = ['$resource'];
+    ReportdailiesService.$inject = ['$resource'];
 
-  function ReportdailiesService($resource) {
-    return $resource('api/reportdailies/:reportdailyId', {
-      reportdailyId: '@_id'
-    }, {
-      update: {
-        method: 'PUT'
-      }
-    });
-  }
+    function ReportdailiesService($resource) {
+        return $resource('api/reportdailies/:reportdailyId', {
+            reportdailyId: '@_id'
+        }, {
+            update: {
+                method: 'PUT'
+            }
+        });
+    }
+    
+    ReportdailiesDayService.$inject = ['$http', '$q'];
+
+    function ReportdailiesDayService($http, $q) {
+        this.getReportDailies = function(reportdate) {
+            var deferred = $q.defer();
+            $http.get('/api/reportdaily/' + reportdate).success(function(report) {
+                deferred.resolve(report);
+            }).error(function(error) {
+                deferred.reject(error);
+            });
+            return deferred.promise;
+        };
+    }
+
 }());
