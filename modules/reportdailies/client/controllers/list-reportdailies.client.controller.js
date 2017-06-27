ce07c2c9088633a36edf0074ff018448ba03fe29
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-06-27 11:04:18 +0700

    update excel

--- modules/reportdailies/client/controllers/list-reportdailies.client.controller.js
+++ modules/reportdailies/client/controllers/list-reportdailies.client.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -14,33 +14,31 @@
         // vm.exportReport = exportReport;
         vm.reportdailies = ReportdailiesService.query();
         vm.reportData = null;
-        vm.showNull = false;
+        vm.startCall = false;
 
         function searchReport(reportDate) {
-            vm.startCall = true;
             var date = new Date(reportDate).getDate();
             var month = new Date(reportDate).getMonth() + 1;
             var year = new Date(reportDate).getFullYear();
             var inputDate = year + "-" + ((month) < 10 ? "0" + month : month) + "-" + (date < 10 ? "0" + date : date);
             console.warn(inputDate);
-            ReportdailiesDayService.getReportDailies(inputDate).then(function(report) {
+            ReportdailiesDayService.getReportDailies(inputDate).then(function (report) {
                 console.log(report);
                 vm.reportData = report;
                 vm.startCall = false;
                 if (vm.reportData.data.length) {
-                    vm.showNull = false;
+                    vm.startCall = true;
                 } else {
-                    vm.showNull = true;
+                    vm.startCall = false;
                 }
-            }, function(error) {
+            }, function (error) {
                 console.error(error);
-                vm.startCall = false;
-                vm.showNull = true;
+                vm.startCall = true;
             });
         }
 
         // function exportReport(report) {
-            // ReportdailiesDayService.exportReportDailies(report);
+        // ReportdailiesDayService.exportReportDailies(report);
         // }
     }
 }());
