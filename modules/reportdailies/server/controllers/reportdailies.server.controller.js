9029bb3804d40f3ac48817a2a37eac7215de77f7
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-07-18 09:35:03 +0700

    update

--- modules/reportdailies/server/controllers/reportdailies.server.controller.js
+++ modules/reportdailies/server/controllers/reportdailies.server.controller.js
@@ -174,7 +174,7 @@ exports.reportdailyByDate = function (req, res, next, reportdate) {
                         if (i.dateTimeIn && i.dateTimeOut) {
                             workhours = workingHoursBetweenDates(i.dateTimeIn, i.dateTimeOut);
                         }
-                        timelate = workingHoursBetweenDates(i.user.employeeprofile.shiftin, i.dateTimeIn);
+                        timelate = workingHoursBetweenDatesLate(i.user.employeeprofile.shiftin, i.dateTimeIn);
                         reportDailyData.push({
                             employeeid: i.user.employeeprofile.employeeid,
                             firstname: i.user.employeeprofile.firstname,
@@ -421,6 +421,25 @@ function deg2rad(deg) {
 }
 // get hours
 function workingHoursBetweenDates(startDateTime, endDateTime) {
+    var start = new Date(startDateTime).getHours() + ":" + new Date(startDateTime).getMinutes();
+    var end = new Date(endDateTime).getHours() + ":" + new Date(endDateTime).getMinutes();
+    start = start.split(":");
+    end = end.split(":");
+    var startDate = new Date(0, 0, 0, start[0], start[1], 0);
+    var endDate = new Date(0, 0, 0, end[0], end[1], 0);
+    var diff = endDate.getTime() - startDate.getTime();
+    var hours = Math.floor(diff / 1000 / 60 / 60);
+    diff -= hours * 1000 * 60 * 60;
+    var minutes = Math.floor(diff / 1000 / 60);
+    // If using time pickers with 24 hours format, add the below line get exact hours
+    if (hours < 0) {
+        hours = hours + 24;
+    }
+
+    return (hours <= 9 ? "0" : "") + hours + ":" + (minutes <= 9 ? "0" : "") + minutes;
+}
+
+function workingHoursBetweenDatesLate(startDateTime, endDateTime) {
     var start = new Date(startDateTime).getHours() + ":" + new Date(startDateTime).getMinutes();
     var end = new Date(endDateTime).getHours() + ":" + new Date(endDateTime).getMinutes();
     start = start.split(":");
