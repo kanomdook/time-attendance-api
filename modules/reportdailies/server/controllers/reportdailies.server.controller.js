4353b492c67c39598ab67a224ff66e45794cc6b2
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-07-18 09:25:37 +0700

    update

--- modules/reportdailies/server/controllers/reportdailies.server.controller.js
+++ modules/reportdailies/server/controllers/reportdailies.server.controller.js
@@ -433,8 +433,9 @@ function workingHoursBetweenDates(startDateTime, endDateTime) {
     var minutes = Math.floor(diff / 1000 / 60);
 
     // If using time pickers with 24 hours format, add the below line get exact hours
-    if (hours < 0)
+    if (hours < 0) {
         hours = hours + 24;
+    }
 
     return (hours <= 9 ? "0" : "") + hours + ":" + (minutes <= 9 ? "0" : "") + minutes;
 }
