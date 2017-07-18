cdad81e54eb65b0f9826b6539bf305e34d8f6d9f
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-07-18 09:29:18 +0700

    update

--- modules/reportdailies/server/controllers/reportdailies.server.controller.js
+++ modules/reportdailies/server/controllers/reportdailies.server.controller.js
@@ -431,11 +431,15 @@ function workingHoursBetweenDates(startDateTime, endDateTime) {
     var hours = Math.floor(diff / 1000 / 60 / 60);
     diff -= hours * 1000 * 60 * 60;
     var minutes = Math.floor(diff / 1000 / 60);
+    console.log(parseInt(start[0]) + " : " + parseInt(end[0]));
+    if (parseInt(start[0]) > parseInt(end[0])) {
+        // If using time pickers with 24 hours format, add the below line get exact hours
+        if (hours < 0) {
+            hours = hours + 24;
+        }
 
-    // If using time pickers with 24 hours format, add the below line get exact hours
-    if (hours < 0) {
-        hours = hours + 24;
+        return (hours <= 9 ? "0" : "") + hours + ":" + (minutes <= 9 ? "0" : "") + minutes;
+    } else {
+        return "0";
     }
-
-    return (hours <= 9 ? "0" : "") + hours + ":" + (minutes <= 9 ? "0" : "") + minutes;
 }
