f167cd5bed5a55ac7f8073edbd42f2053717d58c
kanomdook <kanung.2535@gmail.com>
2018-06-26 10:04:40 +0700

    make it better

--- modules/leaves/server/controllers/leaves.server.controller.js
+++ modules/leaves/server/controllers/leaves.server.controller.js
@@ -150,7 +150,7 @@ exports.listByCompany = function (req, res) {
             var leaveByCompany = [];
             if (leave.length > 0) {
                 leaveByCompany = leave.filter(function (obj) {
-                    console.log(JSON.stringify(obj));
+                    console.log(JSON.stringify(obj.user.employeeprofile));
                     return obj.user.employeeprofile.company ? obj.user.employeeprofile.company._id.toString() : '' === req.user.company ? req.user.company.toString() : '';
                 });
             }
